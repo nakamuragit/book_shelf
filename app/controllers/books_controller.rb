11f6ce866151e5e231aeb92fa9d01736d0c124e2
Nakamura <gireru@hotmail.co.jp>
2019-03-16 14:32:09 +0900

    画像登録機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,9 +20,22 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to @book, notice: "書籍を更新しました。"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def book_params
-    params.require(:book).permit(:title, :price, :publish_date, :description)
+    params.require(:book).permit(:title, :price, :publish_date, :description, :new_image)
   end
 end
