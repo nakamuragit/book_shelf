fa96e8880b4b389d17ab353873cd2f37961e7d23
Nakamura <gireru@hotmail.co.jp>
2019-03-17 09:09:02 +0900

    ログイン機能、設定変更まで完了

--- db/migrate/20190316063551_add_categories_id_to_books.rb
+++ db/migrate/20190316063551_add_categories_id_to_books.rb
@@ -1,5 +1,5 @@
 class AddCategoriesIdToBooks < ActiveRecord::Migration[5.2]
   def change
-    add_reference :books, :category, foreign_key: true
+    add_reference :books, :category, foreign_key: true, index: true, after: :description
   end
 end
